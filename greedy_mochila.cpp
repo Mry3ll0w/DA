26e0c9fca08d41b92c4b8479ea6871e1cbdebca9
Mry3ll0w <48240649+Mry3ll0w@users.noreply.github.com>
2021-10-05 17:23:04 +0200

    greedy_mochila V1
    Mochila sin ordenación, es decir no es óptimo ya que no esta ordenado

--- greedy_mochila.cpp
+++ greedy_mochila.cpp
@@ -32,13 +32,81 @@ struct objeto{
 struct mochila{
     double capacidad;
 };
-std::vector<objeto>& greedy_mochila(std::vector<objeto>& C);
+std::vector<objeto>& greedy_mochila(mochila& m,std::vector<objeto>& C);
 int main(){
     //DATOS DADOS DE INICIO
     std::vector<objeto> obs{{10,0.5},{3,1},{1,1.25}};// en vez de conjunto vector
     mochila m; m.capacidad = 4;
     // llamada al procedimiento greedy
-    
+    std::vector<objeto> conjunto_solucion;
+    conjunto_solucion = greedy_mochila(m,obs);
 
     return 0;
+}
+/*
+mochila : O × c → S
+C ← O
+S ← ∅
+mientras c 6= 0 ∧ C 6= ∅
+    hv, pi ← selecciona-objeto(C)
+    C ← C − {hv, pi}
+    si p ≤ c
+        S ← S ∪ {hv, pi}
+        c ← c − p
+    si no
+        S ← S ∪ {hv · c/p, ci}
+    c ← 0
+*/
+//Funcion de seleccion de objeto candidato perteneciente al conjunto
+objeto seleccion_objeto(std::vector<objeto>& C){
+    double r= -1; // Lo igualamos a -infinito para iterar desde el primer elemento
+    objeto t;
+    for (auto i : C)//Recorremos el conjunto SIN ELIMINAR ya que se hace de forma externa (no competencia de esta funcion)
+    {
+        if ( (i.valor/i.peso) > r)//¿Cual tiene mejor ratio valor/peso??
+        {
+            r = i.valor/i.peso;
+            // {v,p} <- {a, b}
+            t.valor = i.valor;
+            t.peso = i.peso;
+        }
+    }
+    return t;
+}
+
+//Funcion de poda de conjunto (ya que no tenemos la union de los mismos implementado)
+void podar_conjunto(std::vector<objeto>& v,const objeto& o){
+    size_t it=0;
+    while (it<v.size()||(v[it].peso==o.peso && v[it].valor==o.valor))
+    {
+        ++it;
+    }
+    if (v[it].peso==o.peso&& v[it].valor==o.valor)
+    {
+        v.erase(v.begin()+it);
+    }
+    
+}
+std::vector<objeto>& greedy_mochila(mochila &m ,std::vector<objeto>& C){
+    objeto temp_obj;
+    std::vector<objeto>S;
+    while (!C.empty()&& m.capacidad!=0 )//mientras el conjunto no este vacio
+    {
+        temp_obj = seleccion_objeto(C);//Obtenemos el "mejor candidato"
+        std::cout<<"mejor objeto (peso)= "<< temp_obj.peso<<std::endl;
+        podar_conjunto(C,temp_obj);// C <- C - {v.p}
+        if (temp_obj.peso<= m.capacidad)
+        {
+            S.insert(S.end(),temp_obj);// S <- S U {v, p}
+            m.capacidad-=temp_obj.peso;//Actualizamos la capacidad de la mochila 
+        }
+        else{
+            temp_obj.valor=(temp_obj.peso*m.capacidad)/temp_obj.peso;
+            //cuanto valor en funcion del peso disponible nos podemos llevar del objeto
+            temp_obj.peso=m.capacidad;
+            S.insert(S.end(), temp_obj);//metemos el objeto en la mochila
+            m.capacidad=0;//Hemos agotado la maxima capacidad de la mochila 
+        }
+    }
+    
 }

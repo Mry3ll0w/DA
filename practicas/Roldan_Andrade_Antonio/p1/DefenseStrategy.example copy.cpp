b65350e353c7f5fa86789d6d9ff247dade2a5680
Mry3ll0w <48240649+Mry3ll0w@users.noreply.github.com>
2021-10-18 00:28:03 +0200

    Practica 1
    Ejercicio 1, musha grasia junan jose
    1 Asignamos el valor de "calidad" de una celda mediante el calculo de la distancia que separa esa celda a un obstaculo, cuanto mas cerca mejor

--- practicas/Roldan_Andrade_Antonio/p1/DefenseStrategy.example copy.cpp
+++ practicas/Roldan_Andrade_Antonio/p1/DefenseStrategy.example copy.cpp
@@ -18,15 +18,44 @@ RAND_TYPE SimpleRandomGenerator::a;
 #endif
 
 using namespace Asedio;
+// Devuelve la posición en el mapa del centro de la celda (i,j)
+// i - fila
+// j - columna
+// cellWidth - ancho de las celdas
+// cellHeight - alto de las celdas
+Vector3 cellCenterToPosition(int i, int j, float cellWidth, float cellHeight){ 
+    return Vector3((j * cellWidth) + cellWidth * 0.5f, (i * cellHeight) + cellHeight * 0.5f, 0); 
+}
+
+// Devuelve la celda a la que corresponde una posición en el mapa
+// pos - posición que se quiere convertir
+// i_out - fila a la que corresponde la posición pos (resultado)
+// j_out - columna a la que corresponde la posición pos (resultado)
+// cellWidth - ancho de las celdas
+// cellHeight - alto de las celdas
+void positionToCell(const Vector3 pos, int &i_out, int &j_out, float cellWidth, float cellHeight){ 
+    i_out = (int)(pos.y * 1.0f/cellHeight); j_out = (int)(pos.x * 1.0f/cellWidth); 
+}
+
 
 float cellValue(int row, int col, bool** freeCells, int nCellsWidth, int nCellsHeight
 	, float mapWidth, float mapHeight, List<Object*> obstacles, List<Defense*> defenses) {
 	//De forma similar al ejercicio inferior
-    float cellWidth = mapWidth / nCellsWidth;
-    float cellHeight = mapHeight / nCellsHeight;
-    
-    
-    return 0; // implemente aqui la funci�n que asigna valores a las celdas
+    float cellWidth = mapWidth / nCellsWidth; //anchura de la celula
+    float cellHeight = mapHeight / nCellsHeight;//altura de la celula
+    /*Aplicamos el criterio ==> cuanto mas cerca de un obstaculo mejor 
+    */
+    //Usamos el tipo vector 3 para una comparativa (si esta vacia o no de forma mas sencilla)
+    Vector3 t_posicion = cellCenterToPosition(row,col,cellWidth,cellHeight);
+    float value = 0;
+    for(auto i: obstacles){
+        value+=i->position.subtract(t_posicion).length();
+        //value+=_distance(t_position,i.position); 
+        //me da fallos a pesar de que esto representa lo mismo que arriba
+    }
+    //el que tenga menor valor tendra mas obstaculos cerca ==> mayor puntuacion
+    //por tanto lo invierto para tener mayor puntuacion
+    return 1/value; // implemente aqui la funci�n que asigna valores a las celdas
 }
 
 void DEF_LIB_EXPORTED placeDefenses(bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight

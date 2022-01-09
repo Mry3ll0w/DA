c1c41b570ba0a6dfc0ddab679da131d40f83fc1d
Mry3ll0w <aradxc56@gmail.com>
2022-01-09 21:30:21 +0100

    Funciona, falta seguir adaptando el recovery y el algoritmo grande

--- practicas/Roldan_Andrade_Antonio/p4/PathFind.cpp
+++ practicas/Roldan_Andrade_Antonio/p4/PathFind.cpp
@@ -15,86 +15,111 @@
 
 using namespace Asedio;
 
-Vector3 cellCenterToPosition(int i, int j, float cellWidth, float cellHeight){ 
-    return Vector3((j * cellWidth) + cellWidth * 0.5f, (i * cellHeight) + cellHeight * 0.5f, 0); 
+Vector3 cellCenterToPosition(int i, int j, float cellWidth, float cellHeight)
+{
+    return Vector3((j * cellWidth) + cellWidth * 0.5f, (i * cellHeight) + cellHeight * 0.5f, 0);
 }
 
-void DEF_LIB_EXPORTED calculateAdditionalCost(float** additionalCost
-                   , int cellsWidth, int cellsHeight, float mapWidth, float mapHeight
-                   , List<Object*> obstacles, List<Defense*> defenses) {
+
+float bestRelation(List<Defense*> defenses)
+{
+    float best = 0;
+    List<Defense*>::iterator it = defenses.begin();
+    
+    while(it != defenses.end())
+    {
+        if((*it)->damage / (*it)->health > best)
+            best = (*it)->damage / (*it)->health;
+        it++;
+    }
+    return best;
+}
+
+void DEF_LIB_EXPORTED calculateAdditionalCost(float **additionalCost, int cellsWidth, int cellsHeight, float mapWidth, float mapHeight, List<Object *> obstacles, List<Defense *> defenses)
+{
 
     float cellWidth = mapWidth / cellsWidth;
     float cellHeight = mapHeight / cellsHeight;
+    List<Defense*>::iterator it = defenses.begin();
+    float bestDef = bestRelation(defenses), diff;
 
-    for(int i = 0 ; i < cellsHeight ; ++i) {
-        for(int j = 0 ; j < cellsWidth ; ++j) {
+    for (int i = 0; i < cellsHeight; ++i)
+    {
+        for (int j = 0; j < cellsWidth; ++j)
+        {
             Vector3 cellPosition = cellCenterToPosition(i, j, cellWidth, cellHeight);
             float cost = 0;
-            if( (i+j) % 2 == 0 ) {
-                cost = cellWidth * 100;
+            while(it != defenses.end())
+            {
+                cost += (1/_distance(cellPosition, (*it)->position))*1000;
+                diff = bestDef - (bestDef - (*it)->damage / (*it)->health);
+                cost *= diff*100;
+                it++;
             }
-            
+            it = defenses.begin();
             additionalCost[i][j] = cost;
         }
     }
 }
-
 /* -------------------------------------------------------------------------- */
 /*                          IMPLEMENTACIONES PROPIAS                          */
 /* -------------------------------------------------------------------------- */
 
-//Devuelve las posiciones x e y que tiene un punto p en el mapa
-std::pair<int,int> localiza_celdas(const Vector3& p, const float& cellWidth, const float& cellHeight){
-    return std::make_pair((int)(p.x/cellWidth),(int)(p.y/cellHeight));
-}
+std::pair<int,int> get_x_y(const Vector3 pos, float cellWidth, float cellHeight)
+{
+    //i_out = (int)(pos.y * 1.0f / cellHeight);
+    //j_out = (int)(pos.x * 1.0f / cellWidth);
 
-//Para calcular la heuristica usaremos la distancia manhattan entre el nodo origen y el nodo final
-int heuristica_manhattan(const AStarNode* o, const AStarNode* d, const float& cellWidth, const float& cellHeight){
-
-    int row_origen,row_destino,col_origen,col_destino;
-    //Obtenemos la celda en la que estan ambas
-    std::pair<int,int> c_or,c_dest;
-    c_or = localiza_celdas(o->position,cellWidth,cellHeight);   
-    c_dest= localiza_celdas(d->position,cellWidth,cellHeight);
-
-    return abs(c_or.first-c_dest.first)+(c_or.second-c_dest.second); //Devolvemos la distancia existente  
+    return std::make_pair((int)(pos.x * 1.0f / cellWidth), (int)(pos.y /cellHeight));
 
 }
 
-//Busqueda de nodos dentro de una lista de nodos
-bool existe_nodo(AStarNode *node, std::vector<AStarNode *>& lista)
+//Nos sirve para comprobar si el nodo esta o no en la lista de visitados
+bool existe_nodo(AStarNode *nodo, std::vector<AStarNode *> nodos)
 {
     bool found = false;
     
-    for(auto i : lista){
-        if (node == i)
-            found = true; break; // si la encuentra sale de la busqueda
+    for(auto i = nodos.begin(); i != nodos.end() && !found; ++i){
+        
+        if (nodo == *i)
+            found = true;
+        
     }
-
+    
     return found;
 }
 
-//Necesitamos recuperar el camino que se recorre desde el nodo origen al nodo final
-std::list<Vector3> recupera_camino(AStarNode *origen, AStarNode *final)
+List<Vector3> recoverPath(AStarNode *begin, AStarNode *target)
 {
-    AStarNode *it = final;
+    AStarNode *current = target;
+    List<Vector3> path;
 
-    std::list<Vector3> camino;
-
-    camino.push_back(final->position);
-
-    //Recorremos a la inversa el camino comprobando los padres
-    for (AStarNode * it = final ; it->parent != origen && final->parent != NULL; it = it->parent)
+    path.push_back(target->position);
+    while (current->parent != begin && target->parent != NULL)
     {
-        camino.push_front(it->position);
+        current = current->parent;
+        path.push_front(current->position);
     }
-    
-    return camino;
+    return path;
 }
 
-/* -------------------------------------------------------------------------- */
-/*                                cambiar !!!!!                               */
-/* -------------------------------------------------------------------------- */
+int Heuristica_Manhattan(AStarNode *begin, AStarNode *target, float cellWidth, float cellHeight)
+{
+    int o_x, o_y, d_x, d_y, dist = 0;
+    std::pair<int, int> parser;
+    
+    //Coordenadas de origen
+    parser = get_x_y(begin->position, cellWidth, cellHeight);
+    o_x = parser.first; o_y = parser.second;
+
+    //Coordenadas de destino
+    parser = get_x_y(target->position, cellWidth, cellHeight);
+    d_x = parser.first; d_y = parser.second;
+
+    dist = abs(o_x - d_x) + abs(o_y - d_y);
+    return dist;
+}
+
 struct greaterComparator
 {
     bool operator()(AStarNode* a, AStarNode* b)
@@ -103,13 +128,10 @@ struct greaterComparator
     }
 };
 
-
-/* -------------------------------------------------------------------------- */
-/*                          ALGORITMO DE BUSQUEDA A*                          */
-/* -------------------------------------------------------------------------- */
 void DEF_LIB_EXPORTED calculatePath(AStarNode *originNode, AStarNode *targetNode, int cellsWidth, int cellsHeight, float mapWidth, float mapHeight, float **additionalCost, std::list<Vector3> &path)
 {
-
+    /* -------------------------------- VARIABLES ------------------------------- */
+    std::pair<int, int> p;
     int maxIter = 100;
     AStarNode *current = originNode;
     std::vector<AStarNode *> opened, closed;
@@ -118,13 +140,15 @@ void DEF_LIB_EXPORTED calculatePath(AStarNode *originNode, AStarNode *targetNode
     float dist;
     float cellWidth = mapWidth / cellsWidth;
     float cellHeight = mapHeight / cellsHeight;
+    /* -------------------------------- ALGORITMO ------------------------------- */
+    
     targetNode->parent = NULL;
 
     current->G = 0;
-    current->H = heuristica_manhattan(current, targetNode, cellWidth, cellHeight);
-    
-    current->F = current->G + current->H + additionalCost[localiza_celdas(current->position, cellWidth, cellHeight).first]
-    [localiza_celdas(current->position, cellWidth, cellHeight).second];
+    current->H = Heuristica_Manhattan(current, targetNode, cellWidth, cellHeight);
+    //Obtener posiciones dentro de la matriz
+    p = get_x_y(current->position,cellWidth, cellHeight);
+    current->F = current->G + current->H + additionalCost[p.first][p.second];
 
     opened.push_back(current);
 
@@ -149,10 +173,9 @@ void DEF_LIB_EXPORTED calculatePath(AStarNode *originNode, AStarNode *targetNode
                     {
                         (*it)->parent = current;
                         (*it)->G = current->G + _distance(current->position, (*it)->position);
-                        (*it)->H = heuristica_manhattan((*it), targetNode, cellWidth, cellHeight);
+                        (*it)->H = Heuristica_Manhattan((*it), targetNode, cellWidth, cellHeight);
                         
-                        std::pair<int, int> p = localiza_celdas((*it)->position,cellWidth, cellHeight);
-                        
+                        p = get_x_y((*it)->position,cellWidth, cellHeight);
                         (*it)->F = (*it)->G + (*it)->H + additionalCost[p.first][p.second];
                         
                         opened.push_back((*it));
@@ -165,8 +188,10 @@ void DEF_LIB_EXPORTED calculatePath(AStarNode *originNode, AStarNode *targetNode
                         {
                             (*it)->parent = current;
                             (*it)->G += dist;
-                            std::pair<int,int> p = localiza_celdas((*it)->position, cellWidth, cellHeight);
+
+                            p = get_x_y((*it)->position,cellWidth, cellHeight);
                             (*it)->F = (*it)->G + (*it)->H + + additionalCost[p.first][p.second];
+                            
                             std::make_heap(opened.begin(), opened.end(), greaterComparator());
                         }
                     }
@@ -175,5 +200,5 @@ void DEF_LIB_EXPORTED calculatePath(AStarNode *originNode, AStarNode *targetNode
             }
         }
     }
-    path = recupera_camino(originNode, targetNode);
+    path = recoverPath(originNode, targetNode);
 }
